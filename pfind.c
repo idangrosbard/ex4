61b52736444697cc13c075bc9672c756f0af7b2d
idg7 <idg7722@gmail.com>
2022-12-23 21:35:14 +0200

    Code running Cleanup inefficient

--- pfind.c
+++ pfind.c
@@ -1,73 +1,38 @@
+#include <threads.h>
+#include <stdatomic.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 #include <stdio.h>
-#include <thread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
 
 
-/////////////////////////////
-// Thread-safe queue for FIFO thread sync
-
-// Mutex for queue (to make sure only one threads listens to the same condition signal at a time)
-mtx_t mtx;
+// To make sure that different push operations will attempt to wake up different threads, we will use a push_mtx mutex.
+// To make sure that different pop operations will sleep using different cnd_t objects, we will use a pop_mtx mutex.
+mtx_t push_mtx, pop_mtx;
 
 struct thread_sync {
-    mtx_t * mtx;
-    cnd_t * not_empty;
+    mtx_t mtx;
+    cnd_t not_empty;
 };
 
-struct thread_sync_entry {
-    struct thread_sync * sync;
-    struct thread_sync_entry * next;
-};
+static struct thread_sync ** thread_syncs; // Each sleeping thread will wait on a single thread_sync
+static atomic_int num_threads = 0; // The number of thread_syncs
+static atomic_int sleep_head_idx = 0; // The index of the thread_sync that the next push operation will wake up
+static atomic_int sleep_tail_idx = 0; // The index of the thread_sync that the next pop operation will sleep on
+static atomic_int num_sleeping; // The number of currently sleeping threads
 
-struct thread_sync_queue {
-    struct thread_sync_entry * head;
-    struct thread_sync_entry * tail;
-    int num_sleeping_threads;
-};
+static atomic_ullong recognized_files = 0; // Number of files found
+static atomic_int experienced_error; // Whether an error has occurred
+static char * search_term; // The string we're searching for
+//static char * search_root; // The dir we're searchign in
 
-void init(struct thread_sync_queue * q) {
-    q->head = NULL;
-    q->tail = NULL;
-    q->num_sleeping_threads = 0;
-}
 
-void push(struct thread_sync_queue * q, struct thread_sync * sync) {
-    mtx_lock(&mtx);
-    struct thread_sync_entry * e = malloc(sizeof(struct thread_sync_entry));
-    e->sync = sync;
-    e->next = NULL;
-    if (q->head == NULL) {
-        q->head = e;
-        q->tail = e;
-    } else {
-        q->tail->next = e;
-        q->tail = e; 
-    }
-    mtx_unlock(&mtx);
-}
-
-struct thread_sync * pop(struct thread_sync_queue * q) {
-    struct thread_sync_entry * e;
-    struct thread_sync * sync;
-    mtx_lock(&mtx);
-    if (q->head == NULL) {
-        return NULL;
-    }
-    e = q->head;
-    q->head = e->next;
-    if (q->head == NULL) {
-        q->tail = NULL;
-    }
-    sync = e->sync;
-    free(e);
-    mtx_unlock(&mtx);
-    return sync;
-}
-
-struct thread_sync_queue * available_syncs;
-struct thread_sync_queue * waiting_syncs;
-
-/////////////////////////////
-// Thread-safe queue for holding unvisited search paths
+///////////////////////////////
+// A queue to hold the paths yet to be checked
 
 struct entry {
     char * path;
@@ -84,13 +49,21 @@ void init(struct queue * q) {
     q->tail = NULL;
 }
 
-void exit_print(int num_found_files) {
-    printf("Done searching, found %d files\n", num_found_files);
-}
+void push(struct queue * q, char * path) {
+    struct thread_sync * sync;
+    struct entry * e;
+    // First we make sure that if there are sleeping threads, the push will send different wake up signals according to FIFO order
+    mtx_lock(&push_mtx);
+    sync = thread_syncs[sleep_head_idx];
+    mtx_lock(&(sync->mtx));
 
-void push(struct queue * q, char * path, struct thread_sync * sync) {
-    mtx_lock(sync->mtx);
-    struct entry * e = malloc(sizeof(struct entry));
+    // Creating a queue entry and adding it to the queue
+    e = malloc(sizeof(struct entry));
+    if (e == NULL) {
+        fprintf(stderr, "Error allocating memory for queue entry");
+        experienced_error = 1;
+        return;
+    }
     e->path = path;
     e->next = NULL;
     if (q->head == NULL) {
@@ -100,76 +73,228 @@ void push(struct queue * q, char * path, struct thread_sync * sync) {
         q->tail->next = e;
         q->tail = e;
     }
-    cnd_signal(sync->not_empty);
-    mtx_unlock(&sync->mtx);
+    
+    // If there exists a thread that went to sleep, wake it up (we just added a new path to the queue)
+    if (sleep_head_idx != sleep_tail_idx) {
+        cnd_signal(&(sync->not_empty));
+        sleep_head_idx = (sleep_head_idx + 1) % num_threads;
+    }
+
+    mtx_unlock(&(sync->mtx));
+    mtx_unlock(&push_mtx);
 }
 
-char * pop(struct queue * q, struct thread_sync * sync) {
-    mtx_lock(sync->mtx);
-    while (q->head == NULL) {
-        return NULL;
-        cnd_wait(sync->not_empty, sync->mtx);
+void cleanup(struct sync * sync) {
+    atomic_int i;
+    for (i = 0; i < num_threads; i++) {
+        mtx_lock(&(thread_syncs[i]->mtx));
+        cnd_signal(&(thread_syncs[i]->not_empty));
+        mtx_unlock(&(thread_syncs[i]->mtx));
     }
-    struct entry * e = q->head;
+    mtx_unlock(*(sync->mtx));
+    mtx_unlock(&pop_mtx);
+    thrd_exit(0);
+}
+
+char * pop(struct queue * q) {
+    char * path;
+    struct entry * e;
+    struct thread_sync * sync;
+    atomic_int new_sleep_tail_idx;
+    // First we make sure that if there are no paths in the queue, consecutive pops will sleep on different cnd_t objects
+    printf("pop\n");
+    mtx_lock(&pop_mtx);
+    sync = thread_syncs[sleep_tail_idx];
+    mtx_lock(&(sync->mtx));
+    
+    while (q->head == NULL) {
+        printf("Head is null\n");
+        
+        num_sleeping++;
+        // If all threads should be sleeping, we've finished searching files and should exit the program (during cleanup the thread will exit, so num_sleeping won't decrease)
+        if (num_sleeping == num_threads) {
+            cleanup(sync);
+        }
+
+        // Dividing this operation to 2 lines, to make sure that the value in sleep_tail_idx is in [0, num_threads - 1]
+        new_sleep_tail_idx = (sleep_tail_idx + 1) % num_threads;
+        sleep_tail_idx = new_sleep_tail_idx;
+        
+        mtx_unlock(&pop_mtx);
+        cnd_wait(&(sync->not_empty), &(sync->mtx));
+        num_sleeping--;
+    }
+    mtx_unlock(&pop_mtx);
+
+    // Popping the head of the queue
+    e = q->head;
     q->head = e->next;
     if (q->head == NULL) {
         q->tail = NULL;
     }
-    char * path = e->path;
+    path = e->path;
+    printf("Got path %s\n", path);
     free(e);
-    mtx_unlock(&mtx);
+
+    mtx_unlock(&(sync->mtx));
     return path;
+}
+
+/////////////////////////////
+// Functions for the flow of each thread
+
+void iterate_dir(struct queue * q, char* path) {
+    DIR * dir;
+    char * new_path;
+    struct dirent * entry;
+    dir = opendir(path);
+    // If we couldn't open the directory
+    if (dir == NULL) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        experienced_error = 1;
+        return;
+    }
+
+    // Iterating over the directory
+    while ((entry = readdir(dir)) != NULL) {
+        // Ignoring the current and parent directories
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        // Creating a new path to push to the queue
+        new_path = malloc(strlen(path) + strlen(entry->d_name) + 2);
+        if (new_path != NULL) {
+            strcpy(new_path, path);
+            strcat(new_path, "/");
+            strcat(new_path, entry->d_name);
+            push(q, new_path);
+        } else {
+            fprintf(stderr, "Error: malloc failed.\n");
+            experienced_error = 1;
+        }
+    }
+    free(path);
+    closedir(dir);
 }
 
 void thread_scan(struct queue * q) {
     char * path;
+    struct stat st;
+    while(1) {
+        path = pop(q);
+        if (stat(path, &st) == -1) {
+            fprintf(stderr, "%s\n", strerror(errno));
+        }
 
-    path = pop(q);
+        // If the path is to a directory:
+        if (S_ISDIR(st.st_mode)) {
+            // Checking if we can access the directory
+            if (access(path, R_OK | X_OK) == -1) {
+                // We can't access the directory, so we print it
+                if (errno == EACCES) {
+                    printf("Directory %s: Permission denied.\n", path);
+                } else {
+                    printf("Directory '%s' error ", path);
+                    fprintf(stderr, "%s\n", strerror(errno));
+                    experienced_error = 1;
+                }
+            }
 
+            // We can access the directory, so we scan it
+            iterate_dir(q, path);
+        }
+
+        // If the path is to a file, and the file name contains the search term, we print it
+        else {
+            if (strstr(path, search_term) != NULL) {
+                printf("%s\n", path);
+                recognized_files++;
+            }
+        }
+    }
+}
+
+
+///////////////////////////
+// Main thread
+
+void exit_print(int num_found_files) {
+    printf("Done searching, found %d files\n", num_found_files);
 }
 
 int main(int argc, char** argv) {
-    char * search_root_dir, * search_term;
-    int num_threads, cnd_init_return;
+    int i;
     struct queue * q;
+    thrd_t * threads;
+    char * new_path;
     if (argc < 4) {
         fprintf(stderr, "Error: Not enough arguments.");
     }
-    search_root_dir = argv[1];
+    // search_root = argv[1];
     search_term = argv[2];
     num_threads = atoi(argv[3]);
 
     // Check if number of threads is valid
     if (num_threads <= 0) {
         fprintf(stderr, "Error: Invalid number of threads.");
-        exit_print(0);
         exit(1);
     }
 
-    // Initialize mutex
-    if (mtx_init(&mtx, mtx_plain) != thrd_success) {
-        fprintf(stderr, "Error: Mutex initialization failed.");
-        exit_print(0);
+    // Initialize sync variables
+    mtx_init(&push_mtx, mtx_plain);
+    mtx_init(&pop_mtx, mtx_plain);
+
+    experienced_error = 0;
+    
+    thread_syncs = malloc(sizeof(struct thread_sync *) * num_threads);
+    if (thread_syncs == NULL) {
+        fprintf(stderr, "Error: malloc failed.\n");
         exit(1);
     }
-
-    // Initialize condition variable
-    cnd_init_return = cnd_init(&not_empty);
-    if (cnd_init_return != thrd_success) {
-        if (cnd_init_return == thrd_nomem) {
-            fprintf(stderr, "Error: Condition variable initialization failed. Out of memory.");
-        } else if (cnd_init_return == thrd_error) {
-            fprintf(stderr, "Error: Condition variable initialization failed. Unknown error.");
+    
+    threads = malloc(sizeof(thrd_t) * num_threads);
+    for (i = 0; i < num_threads; i++) {
+        thread_syncs[i] = malloc(sizeof(struct thread_sync));
+        if (thread_syncs[i] == NULL) {
+            fprintf(stderr, "Error: malloc failed.\n");
+            exit(1);
         }
-        exit_print(0);
-        exit(1);
+        // Making this a recursive mutex for the cleanup function
+        mtx_init(&(thread_syncs[i]->mtx), mtx_recursive);
+        cnd_init(&(thread_syncs[i]->not_empty));
     }
 
     // Initialize queue
     q = malloc(sizeof(struct queue));
-    init(&q);
-    push(&q, search_root_dir);
+    if (q == NULL) {
+        fprintf(stderr, "Error: malloc failed.\n");
+        exit(1);
+    }
 
+    init(q);
+    new_path = malloc(strlen(argv[1]) + 1);
+    if (new_path != NULL) {
+        strcpy(new_path, argv[1]);
+        push(q, new_path);
+    } else {
+        exit(1);
+    }
+    
+    for (i = 0; i < num_threads; i++) {
+        thrd_create(&threads[i], (thrd_start_t)thread_scan, q);
+    }
 
-    return 0;
+    for (i = 0; i < num_threads; i++) {
+        thrd_join(threads[i], NULL);
+        mtx_destroy(&(thread_syncs[i]->mtx));
+        cnd_destroy(&(thread_syncs[i]->not_empty));
+        free(thread_syncs[i]);
+    }
+    free(thread_syncs);
+    free(threads);
+    free(q);
+    exit_print(recognized_files);
+    
+    return experienced_error;
 }
